f824637bf44f80a17b3dee7d3caaa3dff90d2799
EthanMcKelvey <mckelvey.e@stu.janesville.k12.wi.us>
2024-09-24 09:19:23 -0500

    Initial commit

---
+++ About Me/Abt.Me.py
@@ -0,0 +1,87 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.SystemColors.Highlight
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(235, 9)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(502, 216)
+        self._label1.TabIndex = 0
+        self._label1.Text = "About me"
+        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.SystemColors.Highlight
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(3, 135)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(226, 90)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Show"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.SystemColors.Highlight
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(358, 228)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(214, 90)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.SystemColors.MenuHighlight
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(743, 135)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(218, 90)
+        self._button3.TabIndex = 3
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        self.ClientSize = System.Drawing.Size(963, 445)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "About Me"
+        self.ResumeLayout(False)
+
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def Button1Click(self, sender, e):
+        self._label1.Text = "Hi my name is Ethan and im 14."
+        
+        
+
+    def Button2Click(self, sender, e):
+        self._label1.Text = "I like video games and walking my dogs." 
---
+++ go cougers/go cougers/MainForm.py
@@ -0,0 +1,77 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.Firebrick
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(232, 21)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(498, 164)
+        self._label1.TabIndex = 0
+        self._label1.Text = "My favorite qoute is from Star wars Revenge of the Sith "
+        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.Firebrick
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(47, 261)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(246, 86)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Show"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.Firebrick
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(349, 261)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(252, 86)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.Firebrick
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(643, 261)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(222, 86)
+        self._button3.TabIndex = 3
+        self._button3.Text = "End"
+        self._button3.UseVisualStyleBackColor = False
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        self.ClientSize = System.Drawing.Size(902, 396)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "Favorite Quote"
+        self.ResumeLayout(False)
+
+
+    def Button1Click(self, sender, e):
+     self._label1.Text = "I love you Anikain"
