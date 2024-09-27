6bf57de1fa2e0f5a4e178c1fdbacdfe25ee91906
EthanMcKelvey <mckelvey.e@stu.janesville.k12.wi.us>
2024-09-27 09:02:55 -0500

    About Me

--- Name/Name/MainForm.py
+++ Name/Name/MainForm.py
@@ -17,21 +17,22 @@ class MainForm(Form):
         # 
         # label1
         # 
-        self._label1.BackColor = System.Drawing.SystemColors.WindowFrame
+        self._label1.BackColor = System.Drawing.SystemColors.MenuHighlight
         self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label1.Location = System.Drawing.Point(139, 22)
+        self._label1.Location = System.Drawing.Point(211, 53)
         self._label1.Name = "label1"
-        self._label1.Size = System.Drawing.Size(447, 174)
+        self._label1.Size = System.Drawing.Size(442, 143)
         self._label1.TabIndex = 0
-        self._label1.Text = "Phone Numbers"
+        self._label1.Text = "My Name"
         self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         # 
         # button1
         # 
-        self._button1.BackColor = System.Drawing.Color.Gray
-        self._button1.Location = System.Drawing.Point(44, 232)
+        self._button1.BackColor = System.Drawing.SystemColors.Highlight
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(28, 296)
         self._button1.Name = "button1"
-        self._button1.Size = System.Drawing.Size(143, 65)
+        self._button1.Size = System.Drawing.Size(231, 82)
         self._button1.TabIndex = 1
         self._button1.Text = "Show"
         self._button1.UseVisualStyleBackColor = False
@@ -39,10 +40,11 @@ class MainForm(Form):
         # 
         # button2
         # 
-        self._button2.BackColor = System.Drawing.Color.Gray
-        self._button2.Location = System.Drawing.Point(297, 232)
+        self._button2.BackColor = System.Drawing.SystemColors.Highlight
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(315, 296)
         self._button2.Name = "button2"
-        self._button2.Size = System.Drawing.Size(143, 65)
+        self._button2.Size = System.Drawing.Size(231, 82)
         self._button2.TabIndex = 2
         self._button2.Text = "Clear"
         self._button2.UseVisualStyleBackColor = False
@@ -50,10 +52,11 @@ class MainForm(Form):
         # 
         # button3
         # 
-        self._button3.BackColor = System.Drawing.Color.Gray
-        self._button3.Location = System.Drawing.Point(561, 232)
+        self._button3.BackColor = System.Drawing.SystemColors.Highlight
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(609, 296)
         self._button3.Name = "button3"
-        self._button3.Size = System.Drawing.Size(143, 65)
+        self._button3.Size = System.Drawing.Size(231, 82)
         self._button3.TabIndex = 3
         self._button3.Text = "End"
         self._button3.UseVisualStyleBackColor = False
@@ -61,23 +64,23 @@ class MainForm(Form):
         # 
         # MainForm
         # 
-        self.BackColor = System.Drawing.Color.Maroon
-        self.ClientSize = System.Drawing.Size(765, 333)
+        self.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        self.ClientSize = System.Drawing.Size(852, 418)
         self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
         self.Controls.Add(self._label1)
         self.Name = "MainForm"
-        self.Text = "phone numbers"
+        self.Text = "Name"
         self.ResumeLayout(False)
 
 
     def Button1Click(self, sender, e):
-        self._label1.Text = " Mcdonalds-(608) 752-0100       El Ra Bowl-(608) 757-3020        Taco Bell-(608) 758-2050"
+        self._label1.Text = "Hi my name is Ethan David Mckelvey"
+        
 
     def Button2Click(self, sender, e):
-       self._label1.Text = "Sky Zone- (608) 856-5867             Dave n Busters- (608) 733-2050"
-       
+        self._label1.Text = ""
 
     def Button3Click(self, sender, e):
-        Application.Exit()
+        application.Exit()
--- Schedule/Schedule/MainForm.py
+++ Schedule/Schedule/MainForm.py
@@ -17,67 +17,65 @@ class MainForm(Form):
         # 
         # label1
         # 
-        self._label1.BackColor = System.Drawing.SystemColors.WindowFrame
+        self._label1.BackColor = System.Drawing.Color.DarkRed
         self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._label1.Location = System.Drawing.Point(139, 22)
+        self._label1.Location = System.Drawing.Point(19, 14)
         self._label1.Name = "label1"
-        self._label1.Size = System.Drawing.Size(447, 174)
+        self._label1.Size = System.Drawing.Size(323, 429)
         self._label1.TabIndex = 0
-        self._label1.Text = "Phone Numbers"
-        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        self._label1.Text = "My school Schedule"
         # 
         # button1
         # 
-        self._button1.BackColor = System.Drawing.Color.Gray
-        self._button1.Location = System.Drawing.Point(44, 232)
+        self._button1.BackColor = System.Drawing.Color.DarkRed
+        self._button1.Location = System.Drawing.Point(450, 46)
         self._button1.Name = "button1"
-        self._button1.Size = System.Drawing.Size(143, 65)
+        self._button1.Size = System.Drawing.Size(273, 100)
         self._button1.TabIndex = 1
-        self._button1.Text = "Show"
+        self._button1.Text = "button1"
         self._button1.UseVisualStyleBackColor = False
         self._button1.Click += self.Button1Click
         # 
         # button2
         # 
-        self._button2.BackColor = System.Drawing.Color.Gray
-        self._button2.Location = System.Drawing.Point(297, 232)
+        self._button2.BackColor = System.Drawing.Color.DarkRed
+        self._button2.Location = System.Drawing.Point(450, 185)
         self._button2.Name = "button2"
-        self._button2.Size = System.Drawing.Size(143, 65)
+        self._button2.Size = System.Drawing.Size(273, 100)
         self._button2.TabIndex = 2
-        self._button2.Text = "Clear"
+        self._button2.Text = "button2"
         self._button2.UseVisualStyleBackColor = False
         self._button2.Click += self.Button2Click
         # 
         # button3
         # 
-        self._button3.BackColor = System.Drawing.Color.Gray
-        self._button3.Location = System.Drawing.Point(561, 232)
+        self._button3.BackColor = System.Drawing.Color.DarkRed
+        self._button3.Location = System.Drawing.Point(450, 320)
         self._button3.Name = "button3"
-        self._button3.Size = System.Drawing.Size(143, 65)
+        self._button3.Size = System.Drawing.Size(273, 100)
         self._button3.TabIndex = 3
-        self._button3.Text = "End"
+        self._button3.Text = "button3"
         self._button3.UseVisualStyleBackColor = False
         self._button3.Click += self.Button3Click
         # 
         # MainForm
         # 
-        self.BackColor = System.Drawing.Color.Maroon
-        self.ClientSize = System.Drawing.Size(765, 333)
+        self.BackColor = System.Drawing.Color.Black
+        self.ClientSize = System.Drawing.Size(825, 460)
         self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
         self.Controls.Add(self._label1)
         self.Name = "MainForm"
-        self.Text = "phone numbers"
+        self.Text = "Schedule"
         self.ResumeLayout(False)
 
 
     def Button1Click(self, sender, e):
-        self._label1.Text = " Mcdonalds-(608) 752-0100       El Ra Bowl-(608) 757-3020        Taco Bell-(608) 758-2050"
+        self._label1.Text = "My first five classes:        Biology                 Computer Programing  Graphic Communications  English                    World Studies/History" 
 
     def Button2Click(self, sender, e):
-       self._label1.Text = "Sky Zone- (608) 856-5867             Dave n Busters- (608) 733-2050"
-       
+        self._label1.Text = "After the Fifth Class I have:                            Lunch                     Algebra                      Freshman Seminar                 Spanish"
 
     def Button3Click(self, sender, e):
         Application.Exit()
