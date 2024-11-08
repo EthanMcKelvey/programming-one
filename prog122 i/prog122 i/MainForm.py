41f673c16854fc81675483f563a00d23222e4b13
EthanMcKelvey <mckelvey.e@stu.janesville.k12.wi.us>
2024-11-08 09:37:02 -0600

    programing 1

---
+++ prog122 i/prog122 i/MainForm.py
@@ -0,0 +1,74 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # listBox1
+        # 
+        self._listBox1.BackColor = System.Drawing.SystemColors.InactiveBorder
+        self._listBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.ItemHeight = 33
+        self._listBox1.Location = System.Drawing.Point(12, 12)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(1025, 367)
+        self._listBox1.TabIndex = 0
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.DodgerBlue
+        self._button1.Location = System.Drawing.Point(41, 401)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(245, 78)
+        self._button1.TabIndex = 1
+        self._button1.Text = "button1"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.DodgerBlue
+        self._button2.Location = System.Drawing.Point(388, 401)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(245, 78)
+        self._button2.TabIndex = 2
+        self._button2.Text = "button2"
+        self._button2.UseVisualStyleBackColor = False
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.DodgerBlue
+        self._button3.Location = System.Drawing.Point(727, 401)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(245, 78)
+        self._button3.TabIndex = 3
+        self._button3.Text = "button3"
+        self._button3.UseVisualStyleBackColor = False
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.Navy
+        self.ClientSize = System.Drawing.Size(1049, 505)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._listBox1)
+        self.Name = "MainForm"
+        self.Text = "prog122 i"
+        self.ResumeLayout(False)
+
+
+    def Button1Click(self, sender, e):
+        pass
---
+++ prog122A/prog122A/MainForm.py
@@ -0,0 +1,59 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._button1 = System.Windows.Forms.Button()
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self.SuspendLayout()
+        # 
+        # button1
+        # 
+        self._button1.Location = System.Drawing.Point(23, 370)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(204, 98)
+        self._button1.TabIndex = 0
+        self._button1.Text = "button1"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # listBox1
+        # 
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.Location = System.Drawing.Point(23, 61)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(956, 303)
+        self._listBox1.TabIndex = 1
+        # 
+        # textBox1
+        # 
+        self._textBox1.Location = System.Drawing.Point(51, 8)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(328, 20)
+        self._textBox1.TabIndex = 2
+        # 
+        # MainForm
+        # 
+        self.ClientSize = System.Drawing.Size(1004, 500)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._listBox1)
+        self.Controls.Add(self._button1)
+        self.Name = "MainForm"
+        self.Text = "prog122A"
+        self.ResumeLayout(False)
+        self.PerformLayout()
+
+
+    def Button1Click(self, sender, e):
+        num = int(self._textBox1.Text)
+        
+        
+        
+        num = int(self.listBox1.Text)
