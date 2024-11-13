405876752b0a677ec29f5fa9dedd5be8b63006e5
EthanMcKelvey <mckelvey.e@stu.janesville.k12.wi.us>
2024-11-13 09:35:47 -0600

    programming1

--- prog 152a/prog 152a/MainForm.py
+++ prog 152a/prog 152a/MainForm.py
@@ -22,27 +22,28 @@ class MainForm(Form):
         self._listBox1.ForeColor = System.Drawing.Color.Fuchsia
         self._listBox1.FormattingEnabled = True
         self._listBox1.ItemHeight = 20
-        self._listBox1.Location = System.Drawing.Point(25, 12)
+        self._listBox1.Location = System.Drawing.Point(12, 12)
         self._listBox1.Name = "listBox1"
-        self._listBox1.Size = System.Drawing.Size(897, 284)
+        self._listBox1.Size = System.Drawing.Size(1034, 364)
         self._listBox1.TabIndex = 0
         # 
         # button1
         # 
         self._button1.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        self._button1.Location = System.Drawing.Point(25, 329)
+        self._button1.Location = System.Drawing.Point(25, 418)
         self._button1.Name = "button1"
-        self._button1.Size = System.Drawing.Size(198, 83)
+        self._button1.Size = System.Drawing.Size(221, 93)
         self._button1.TabIndex = 1
         self._button1.Text = "button1"
         self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
         # 
         # button2
         # 
         self._button2.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        self._button2.Location = System.Drawing.Point(362, 329)
+        self._button2.Location = System.Drawing.Point(427, 418)
         self._button2.Name = "button2"
-        self._button2.Size = System.Drawing.Size(198, 83)
+        self._button2.Size = System.Drawing.Size(221, 93)
         self._button2.TabIndex = 2
         self._button2.Text = "button2"
         self._button2.UseVisualStyleBackColor = False
@@ -50,9 +51,9 @@ class MainForm(Form):
         # button3
         # 
         self._button3.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        self._button3.Location = System.Drawing.Point(659, 329)
+        self._button3.Location = System.Drawing.Point(816, 418)
         self._button3.Name = "button3"
-        self._button3.Size = System.Drawing.Size(198, 83)
+        self._button3.Size = System.Drawing.Size(221, 93)
         self._button3.TabIndex = 3
         self._button3.Text = "button3"
         self._button3.UseVisualStyleBackColor = False
@@ -60,7 +61,7 @@ class MainForm(Form):
         # MainForm
         # 
         self.BackColor = System.Drawing.SystemColors.HotTrack
-        self.ClientSize = System.Drawing.Size(960, 438)
+        self.ClientSize = System.Drawing.Size(1058, 523)
         self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
@@ -69,3 +70,6 @@ class MainForm(Form):
         self.Text = "prog 152a"
         self.ResumeLayout(False)
 
+
+    def Button1Click(self, sender, e):
+        
